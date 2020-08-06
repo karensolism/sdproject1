57846cfa30e40fe9c802c2ac318d64c4234dd5f0
karensolism <61286131+karensolism@users.noreply.github.com>
2020-08-06 12:05:08 -0500

    actualizacion
    a

---
+++ database/migrations/2020_08_06_092639_create_roles_table.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        /*Schema::table('roles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('label')->nullable();
+            $table->timestamps();
+            
+        });
+
+        Schema::table('rol_user', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('role_id');
+            $table->timestamps();
+
+            $table->foreign('user_id')
+            ->references(id)
+            ->on('users')
+            ->onDelete('casacda'); 
+
+             $table->foreign('role_id')
+            ->references(id)
+            ->on('roles')
+            ->onDelete('casacda'); 
+          
+        });*/
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('roles', function (Blueprint $table) {
+            //
+        });
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Auth::routes();
+//ruta principal
+Route::get('/', 'HomeController@index')->name('home');
+Route::resource('usuarios', 'UserController');
+
