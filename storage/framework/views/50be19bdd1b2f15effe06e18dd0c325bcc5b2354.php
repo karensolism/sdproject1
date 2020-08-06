3ebb676300810659e3e35b8d2857a857cd57cade
karensolism <61286131+karensolism@users.noreply.github.com>
2020-08-05 23:08:45 -0500

    test

---
+++ storage/framework/views/50be19bdd1b2f15effe06e18dd0c325bcc5b2354.php
@@ -0,0 +1,27 @@
+
+
+<?php $__env->startSection('content'); ?>
+<table class="table table-striped table-dark">
+  <thead>
+    <tr>
+      <th scope="col">ID</th>
+      <th scope="col">Nombre</th>
+      <th scope="col">Correo</th>
+     
+    </tr>
+  </thead>
+  <tbody>
+    <?php $__currentLoopData = $users; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $users): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+    <tr>
+     
+      <th scope="row"><?php echo e($users->id); ?></th>
+      <td><?php echo e($users->name); ?></td>
+      <td><?php echo e($users->email); ?></td>
+      
+    </tr>
+    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+  </tbody>
+</table>
+
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\ADMIN\Documents\GitHub\sdproject\resources\views/usuarios.blade.php ENDPATH**/ ?>
